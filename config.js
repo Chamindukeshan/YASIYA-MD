6f9bf3182187213ed8d08e5ad83d9c75e1944f70
Chamindukeshan <Chamindukeshankeshan233@gmail.com>
2025-11-19 19:00:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?LW6Cr1'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?cK6US5'
 };
